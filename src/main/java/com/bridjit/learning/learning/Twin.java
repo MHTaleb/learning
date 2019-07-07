ae0d78594147f2c34814463cb93be5058ca68205
MHTaleb <talcorpdz@gmail.com>
2019-07-07 15:19:44 +0100

    twin exercice completed

---
+++ src/main/java/com/bridjit/learning/learning/Twin.java
@@ -0,0 +1,27 @@
+package com.bridjit.learning.learning;
+
+import java.util.Arrays;
+
+public class Twin {
+
+	/**
+	 * is twin check if all letters in two strings are the same ( not case sensitive, letters must be same count )
+	 * */
+	public static boolean isTwin(String word1, String word2) {
+
+		if (word1 != null && word2 != null) {
+			word1 = word1.replaceAll("\\s+", "").toLowerCase();
+			word2 = word2.replaceAll("\\s+", "").toLowerCase();
+			if (word1.length() == word2.length()) {
+				char[] word1letters = word1.toCharArray();
+				char[] word2letters = word2.toCharArray();
+				Arrays.parallelSort(word1letters);
+				Arrays.parallelSort(word2letters);
+				return Arrays.equals(word1letters, word2letters);
+			}
+		}
+		
+		return false;
+	}
+
+}
---
+++ src/test/java/com/bridjit/learning/learning/TwinTest.java
@@ -0,0 +1,51 @@
+package com.bridjit.learning.learning;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TwinTest  extends TestCase{
+	public TwinTest(String testName) {
+		super(testName);
+	}
+	
+	public static Test suite()
+    {
+        return new TestSuite( TwinTest.class );
+    }
+	
+	/**
+	 * check twin is true for abc bca 
+	 * */
+	public void testABCandBCA() {
+		assertTrue(Twin.isTwin("abc","bca"));
+	}
+	
+	/**
+	 * check twin is false for null bca 
+	 * */
+	public void testNullandBCA() {
+		assertFalse(Twin.isTwin(null,"bca"));
+	}
+	
+	/**
+	 * check twin is true for aBc bca 
+	 * */
+	public void testCaseSensitive() {
+		assertTrue(Twin.isTwin("abC","bca"));
+	}
+	
+	/**
+	 * check twin is true for "abc  " "bca" 
+	 * */
+	public void testTrim() {
+		assertTrue(Twin.isTwin("abc  ","bca"));
+	}
+	
+	/**
+	 * check twin is true for "a  bc" "bca" 
+	 * */
+	public void testSpace() {
+		assertTrue(Twin.isTwin("a  bc","bca"));
+	}
+}
